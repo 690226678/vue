3e8b26e39ed5ad5f97ca0e35db03803d12b5cb0a
lei <690226678@qq.com>
2020-04-28 16:07:57 +0800

    修改生产环境 APP_BASE_API

--- src/api/user.js
+++ src/api/user.js
@@ -2,7 +2,7 @@ import request from '@/utils/request'
 
 export function login(data) {
   return request({
-    url: '/api/admin/login',
+    url: '/api/console/login',
     method: 'post',
     data
   })
